6b963fc1f0dfe56b7e31b614fb3979e22e47350a
Kawis77 <krzysztofsiwak77@gmail.com>
2023-03-03 22:42:16 +0100

    first commit with basic configuration

---
+++ src/test/java/com/example/ks_demo/KsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ks_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
